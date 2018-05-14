503f5e137332413edb89078b20e1e9c47074aa5d
maksim-maslov <brat.maksim@gmail.com>
2018-05-14 23:33:37 +0300

    Добавил решение доп задания

--- app/src/EditUser/EditUserCtrl.js
+++ app/src/EditUser/EditUserCtrl.js
@@ -4,17 +4,17 @@ userApp.controller('EditUserCtrl', function ($scope, $routeParams, UsersService)
 
   $scope.userLoaded = false;
   
-  $scope.user = {}
+  $scope.user = {};
 
   UsersService.getUser($routeParams['userId']).then(function (response) {
     $scope.user = response.data;
     $scope.userLoaded = true;
-  })  
+  });
 
   $scope.editUser = function (myUser) {
 
     UsersService.editUser(myUser, $scope.user.id).then(function (response) {
       console.log(response);
-    })
-  }
-})
+    });
+  };
+});
--- app/src/UserList/UserListCtrl.js
+++ app/src/UserList/UserListCtrl.js
@@ -1,18 +1,27 @@
 'use strict'
 
-userApp.controller('UserListCtrl', function ($scope, UsersService, PostsService) {
+userApp.controller('UserListCtrl', function ($scope, $q, UsersService, PostsService) {
   
-  $scope.userLoaded = false
+  $scope.userLoaded = false;
 
-  UsersService.getUsers().then(function (response) {
-    $scope.users = response.data
-    $scope.userLoaded = true
-  })
+  // UsersService.getUsers().then(function (response) {
+  //   $scope.users = response.data
+  //   $scope.userLoaded = true
+  // })
 
-  PostsService.getPosts().then(function (response) {
-    $scope.posts = response.data
-    $scope.userLoaded = true
-  })
+  // PostsService.getPosts().then(function (response) {
+  //   $scope.posts = response.data
+  //   $scope.userLoaded = true
+  // })
+
+  $q.all({
+    obj1: UsersService.getUsers(),
+    obj2: PostsService.getPosts()
+  }).then(function(values){
+    $scope.users = values.obj1.data;
+    $scope.posts = values.obj2.data;
+    $scope.userLoaded = true;
+  });
 
 
 /*   UsersService.getUsers().then(function (response) {
